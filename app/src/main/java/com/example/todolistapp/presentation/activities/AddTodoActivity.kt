b63f45545506f36f170c36fd2d87aea086d02611
viktoriurkov <VVYurkov@alfabank.ru>
2022-08-01 15:17:25 +0300

    feat(deploy): annotate second activity

--- app/src/main/java/com/example/todolistapp/presentation/activities/AddTodoActivity.kt
+++ app/src/main/java/com/example/todolistapp/presentation/activities/AddTodoActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.viewModels
 import com.example.todolistapp.data.models.Todo
 import com.example.todolistapp.databinding.ActivityAddTodoBinding
 import com.example.todolistapp.presentation.viewModel.TodoViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AddTodoActivity : AppCompatActivity() {
     private val viewModel: TodoViewModel by viewModels()
     lateinit var binding: ActivityAddTodoBinding
